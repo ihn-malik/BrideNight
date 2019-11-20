794c33bf18db7f8e242667641db77e9cc96bbc3c
mac <mac@iMac-alkhas-b-mac.local>
2019-11-20 18:32:04 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import {AppRegistry} from 'react-native';
+
+import App from './app/App';
+
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+
+
+
+
+
+
+
+
